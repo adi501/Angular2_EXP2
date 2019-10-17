ec513becde3688bc38c4fe83bd05ddfbe0911b9d
adi501 <jc.adi101@gmail.com>
2019-10-17 14:50:12 +0530

    aa

---
+++ src/app/Components_EXP2/Components_EXP2.Component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "Components_EXP2",
+  template: "{{Name}}"
+})
+export class Components_EXP2 {
+  Name: string = "Components_EXP2";
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,24 @@ import { AppComponent } from "./app.component";
 import { EmployeeComponent } from "./Employee/Employee.component";
 import { EXP1 } from "./EXP1/EXP1.Component";
 import { Components_EXP1 } from "./components_EXP1/Components_EXP1.component";
+import { Components_EXP2 } from "./Components_EXP2/Components_EXP2.Component";
 
 @NgModule({
-  declarations: [AppComponent, EmployeeComponent, EXP1, Components_EXP1],
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    EXP1,
+    Components_EXP1,
+    Components_EXP2
+  ],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent, EmployeeComponent, EXP1, Components_EXP1]
+  bootstrap: [
+    AppComponent,
+    EmployeeComponent,
+    EXP1,
+    Components_EXP1,
+    Components_EXP2
+  ]
 })
 export class AppModule {}
